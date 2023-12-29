a59480efbe164c27427859d84ff9b8e2a334672f
thatmarcel <contact@thatmarcel.com>
2023-12-29 15:44:51 +0100

    Minor formatting tweaks to our SRWebSocketDelegate

--- Controller/BPSocketConnectionManager+SRWebSocketDelegate.m
+++ Controller/BPSocketConnectionManager+SRWebSocketDelegate.m
@@ -3,21 +3,21 @@
 #import "./Logging.h"
 
 @implementation BPSocketConnectionManager (SRWebSocketDelegate)
-    - (void) webSocketDidOpen:(SRWebSocket *)webSocket {
+    - (void) webSocketDidOpen:(SRWebSocket*)webSocket {
         [self sendBeginMessage];
     }
     
-    - (void) webSocket:(SRWebSocket *)webSocket didFailWithError:(NSError *)error {
+    - (void) webSocket:(SRWebSocket*)webSocket didFailWithError:(NSError*)error {
         [self handleConnectionError: error];
     }
     
-    - (void) webSocket:(SRWebSocket *)webSocket didReceiveMessageWithString:(NSString *)message {
+    - (void) webSocket:(SRWebSocket*)webSocket didReceiveMessageWithString:(NSString*)message {
         LOG(@"Received string message: %@", message);
         
-        NSData *messageData = [message dataUsingEncoding: NSUTF8StringEncoding];
+        NSData* messageData = [message dataUsingEncoding: NSUTF8StringEncoding];
         
-        NSError *jsonDecodingError;
-        NSDictionary *jsonContents = [NSJSONSerialization JSONObjectWithData: messageData options: 0 error: &jsonDecodingError];
+        NSError* jsonDecodingError;
+        NSDictionary* jsonContents = [NSJSONSerialization JSONObjectWithData: messageData options: 0 error: &jsonDecodingError];
         
         if (jsonDecodingError) {
             LOG(@"Couldn't parse text message as JSON: %@", jsonDecodingError);
@@ -27,12 +27,12 @@
         [self handleReceivedMessageWithContents: jsonContents];
     }
     
-    - (void) webSocket:(SRWebSocket *)webSocket didReceiveMessageWithData:(NSData *)data {
+    - (void) webSocket:(SRWebSocket*)webSocket didReceiveMessageWithData:(NSData*)data {
         LOG(@"Ignoring received data message");
     }
     
-    - (void) webSocket:(SRWebSocket *)webSocket didCloseWithCode:(NSInteger)code reason:(NSString *)reason wasClean:(BOOL)wasClean {
-        NSDictionary *userInfo = @{
+    - (void) webSocket:(SRWebSocket*)webSocket didCloseWithCode:(NSInteger)code reason:(NSString*)reason wasClean:(BOOL)wasClean {
+        NSDictionary* userInfo = @{
             @"Error Reason": [NSString stringWithFormat: @"webSocket closed with reason: %@", reason]
         };
         [self handleConnectionError: [NSError errorWithDomain: kSuiteName code: code userInfo: userInfo]];
