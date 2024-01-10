379abd11977f0b23c709de093150a3f74a56b20c
thatmarcel <contact@thatmarcel.com>
2024-01-10 20:43:48 +0100

    Add time to log entries

--- Shared/Logging.m
+++ Shared/Logging.m
@@ -12,8 +12,12 @@ void bp_log_impl(NSString* moduleName, NSString* logString) {
         [fileManager createFileAtPath: kLogFilePath contents: nil attributes: nil];
     }
     
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat: @"HH:mm:ss"];
+    NSString *dateString = [dateFormatter stringFromDate: [NSDate date]];
+    
     NSFileHandle* fileHandle = [NSFileHandle fileHandleForWritingAtPath: kLogFilePath];
     [fileHandle seekToEndOfFile];
-    [fileHandle writeData: [[NSString stringWithFormat: @"[%@] %@\n", moduleName, [logString stringByReplacingOccurrencesOfString: @"\n" withString: @" "]] dataUsingEncoding: NSUTF8StringEncoding]];
+    [fileHandle writeData: [[NSString stringWithFormat: @"%@: [%@] %@\n", dateString, moduleName, [logString stringByReplacingOccurrencesOfString: @"\n" withString: @" "]] dataUsingEncoding: NSUTF8StringEncoding]];
     [fileHandle closeFile];
 }
